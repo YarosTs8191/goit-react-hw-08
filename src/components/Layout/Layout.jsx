80d5fa9d32e0cc5416d7203c0e0e47944d06dcbf
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-06-14 11:28:10 +0200

    add-css-module

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import AppBar from "../AppBar/AppBar";
+import css from "./Layout.module.css";
 
 const Layout = () => {
   return (
     <>
       <AppBar /> {/* Навігаційна панель */}
-      <main>
+      <main className={css.container}>
         <Outlet /> {/* Виводить внутрішні сторінки */}
       </main>
     </>
